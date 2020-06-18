216257064d0e58fe1966d79412b796c86c4c867c
Johnnyevans32 <evanseburu32@gmail.com>
2020-06-18 10:31:11 +0100

    Initial commit

--- piggyvest/js/style.js
+++ piggyvest/js/style.js
@@ -1,39 +1,46 @@
 function calc(){
 	
-	let amount = parseInt(document.getElementById('amount').value);
-
-	let time = parseInt(document.getElementById('period').value);
-
-	let plan = document.getElementById('plan').value;
-
-	let text = "Your interest amount is ";
-
-	let currency = " NAIRA";
-
-
-	if (plan  === 'Piggybank') {
-		let interest = 10;
-		document.getElementById('result').innerHTML = text + (amount *  time * interest)/100 + currency;
-	}
-	if (plan  === 'Safelock') {
-		let interest = 15.5;
-		document.getElementById('result').innerHTML = text +  (amount *  time * interest)/100  + currency;
-	}
-	if (plan  === 'Target') {
-		let interest = 10;
-		document.getElementById('result').innerHTML = text +  (amount *  time * interest)/100 + currency;
-	}
-	if (plan  === 'Flex') {
-		let interest = 10;
-		document.getElementById('result').innerHTML = text +  (amount *  time * interest)/100 + currency;
-	}
-	if (plan  === 'Flex_Dollar') {
-		let interest = 6;
-		document.getElementById('result').innerHTML = text + (amount *  time * interest)/100 + currency;
-	}
-	if (plan  === 'Investify') {
-		let interest = 25;
-		document.getElementById('result').innerHTML = text + (amount *  time * interest)/100 + currency;
-	}
+	let money= document.getElementById('amount').value;
 	
+	if (money === "") {
+		document.getElementById('result').innerHTML = "Enter the amount you wish to invest/save!! ";
+	}else{
+		let amount = parseInt(money)
+
+		let time = parseInt(document.getElementById('period').value);
+
+		let plan = document.getElementById('plan').value;
+
+		let text = "Your interest amount is ";
+
+		let currency = " NAIRA After ";
+
+		let month = "Months"
+
+		if (plan  === 'Piggybank') {
+			let interest = 10;
+			document.getElementById('result').innerHTML = text + (amount *  time * interest)/100 + currency + (time * 12) + month;
+		}
+		if (plan  === 'Safelock') {
+			let interest = 15.5;
+			document.getElementById('result').innerHTML = text +  (amount *  time * interest)/100  + currency + (time * 12) + month;
+		}
+		if (plan  === 'Target') {
+			let interest = 10;
+			document.getElementById('result').innerHTML = text +  (amount *  time * interest)/100 + currency + (time * 12) + month;
+		}
+		if (plan  === 'Flex') {
+			let interest = 10;
+			document.getElementById('result').innerHTML = text +  (amount *  time * interest)/100 + currency + (time * 12) + month;
+		}
+		if (plan  === 'Flex_Dollar') {
+			let interest = 6;
+			document.getElementById('result').innerHTML = text + (amount *  time * interest)/100 + currency + (time * 12) + month;
+		}
+		if (plan  === 'Investify') {
+			let interest = 25;
+			document.getElementById('result').innerHTML = text + (amount *  time * interest)/100 + currency + (time * 12) + month;
+		}
+		
+	}	
 }
